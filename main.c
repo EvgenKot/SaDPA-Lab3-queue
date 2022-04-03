80cf36ba9b08a4ced80e8b5afce2ff082775c3d8
Evgeny Kot <ukal.evgen@gmail.com>
2022-04-03 23:07:01 +0500

    v0.9
    Рабочий варик

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 
-
-
 /*
 Ращупкин Евгений КЭ-203
 28 вариант
@@ -19,47 +17,41 @@ typedef struct Node {
 } Node;
 
 
-void push(Node **head, int data) {
-    Node *tmp = (Node*) malloc(sizeof(Node));
-    tmp->value = data;
-    tmp->next = (*head);
-    (*head) = tmp;
-}
+typedef struct Queue 
+{
+    Node *head;
+    Node *tail;
+
+    size_t size;
+} Queue;
 
 
-
-void pushBack(Node *head, Node *tail, int data) {
+void pushBack(Queue* q, int data) {
+    q -> size ++;
     Node *newNode = (Node*) malloc(sizeof(Node));
     newNode->value = data;
     newNode->next = NULL;
-    if (head == NULL) head = newNode;
-    else tail->next = newNode;
 
-    tail = newNode;
+    if (q->head == NULL) q->head = newNode;
+    else q->tail->next = newNode;
+    q->tail = newNode;
 }
 
-int pop(Node **head) {
+
+int pop(Queue* q) {
+    q -> size --;
     Node* prev = NULL;
     int val;
-    if (head == NULL) {
+    if (q->head == NULL) {
         exit(-1);
     }
-    prev = (*head);
+    prev = q->head;
     val = prev->value;
-    (*head) = (*head)->next;
+    q->head = q->head->next;
     free(prev);
     return val;
 }
 
-
-void fromArrayBack(Node **head, Node **tail, int *arr, size_t size) {
-    int i = size;
-    while (i != -1) {
-        pushBack(*head, *tail, arr[i]);
-        i--;
-    }
-
-}
 
 void printQueue(Node* head) {
 
@@ -72,22 +64,88 @@ void printQueue(Node* head) {
 }
 
 
-
-
-
 void main() {
-    Node* head = NULL;
-    Node* tail = head;
+    Queue q1;
+    q1.head = NULL;
+    q1.tail = q1.head;
+    q1.size = 0;
 
-    push(&head, 5);
-    push(&head, 6);
-    pushBack(head, tail, 28);
-    //pushBack(head, tail, 20);
-    //pushBack(head, tail, 0);
-    //pushBack(head, tail, -1);
-    //int arr[] = {10, 1, 10,-3,4,5,-6,7,8,0,15};
-    //fromArrayBack(&head, arr, 11);
+    Queue q2;
+    q2.head = NULL;
+    q2.tail = q2.head;
+    q2.size = 0;
+
+    pushBack(&q1, 28);
+    pushBack(&q1, 20);
+    pushBack(&q1, -5);
+    pushBack(&q1, 0);
+    pushBack(&q1, 11);
+    pushBack(&q1, -4);
+    pushBack(&q1, -1);
+    pushBack(&q1, 24);
+    pushBack(&q1, -28);
+    printf("[%d] size of queue \n ", q1.size);
+
+    printQueue(q1.head);
+
+    int data;
+    int n = 0;
+    int p = 0;
+
+    int nmax = 0;
+    int pmin = 0;
 
 
-    printQueue(head);
+    while (q1.head != NULL){
+        data = pop(&q1);
+        if (data > 0)
+        {
+           if (data < pmin){  
+                pmin = data;
+                p = 0;
+                printf(" %d new positive min \n ", pmin);
+            }
+            else if (pmin == 0){
+                pmin = data;
+                p = 0;
+                printf("%d first positive min \n ", pmin);
+            }
+        }
+        else if (data < 0){
+            if (data > nmax){  
+                nmax = data;
+                n = 0;
+                printf(" %d new negative max \n ", nmax);
+            }
+            else if (nmax == 0){
+                nmax = data;
+                n = 0;
+                printf("%d first negative max \n ", nmax);
+            }
+        }
+        n++;
+        p++;
+
+        pushBack(&q2, data);      
+    }
+
+    printf("%d positive min on -%d position \n %d negative max on -%d position\n", pmin, p, nmax, n);
+    printQueue(q2.head);
+
+    p--;
+    n--;
+    while (q2.head != NULL){
+        data = pop(&q2);
+        if (q2.size == n){
+            pushBack(&q1, pmin);
+        }
+        else if (q2.size == p){
+            pushBack(&q1, nmax);
+        }
+        else{
+            pushBack(&q1, data);
+        }     
+    }
+    printQueue(q1.head);
+
 }
